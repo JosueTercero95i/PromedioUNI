3dc1b6b356b13567ca872888ae3c63cf13e08a1b
JosueTercero95i <167829608+JosueTercero95i@users.noreply.github.com>
2024-05-12 13:14:16 -0600

    initial commit

---
+++ UNIGUIA4/Alumnos.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public partial class Universidad
+{
+    private List<Alumno> ListaAlumnos;
+
+    public Universidad()
+    {
+        ListaAlumnos = new List<Alumno>();
+    }
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        ListaAlumnos.Add(alumno);
+    }
+
+    public List<Alumno> ObtenerListaAlumnos()
+    {
+        return ListaAlumnos;
+    }
+}
+public partial class Alumno
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    private string NumeroCarnet { get; set; }
+    public Carreras Carrera { get; set; }
+    public List<double> Calificaciones { get; set; }
+
+    public Alumno(string nombre, int edad, Carreras carrera, string numeroCarnet)
+    {
+        Nombre = nombre;
+        Edad = edad;
+        Carrera = carrera;
+        NumeroCarnet = numeroCarnet;
+        Calificaciones = new List<double>();
+    }
+
+    public double CalcularPromedio()
+    {
+        if (Calificaciones.Count == 0)
+        {
+            return 0;
+        }
+
+        double SumaCalificaciones = 0;
+        foreach (var Calificacion in Calificaciones)
+        {
+            SumaCalificaciones += Calificacion;
+        }
+        return SumaCalificaciones / Calificaciones.Count;
+    }
+
+    public string EstadoAprobacion()
+    {
+        double promedio = CalcularPromedio();
+        if (promedio >= 60)
+        {
+            return "aprobado";
+        }
+        else
+        {
+            return "desaprobado";
+        }
+    }
+
+    public void AgregarCalificacion(double calificacion)
+
+    {
+        Calificaciones.Add(calificacion);
+    }
+
+
+    public string ObtenerNumeroCarnet()
+    {
+        return NumeroCarnet;
+    }
+
+    public void AsignarNumeroCarnet(string numeroCarnet)
+    {
+        NumeroCarnet = numeroCarnet;
+    }
+}
+
+public enum Carreras
+{
+    Ing_Sistemas,
+    Ing_Computacion,
+    Ing_Telecomunicaciones,
+    Ing_Electronica
+}
+
+
---
+++ UNIGUIA4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Universidad universidad = new Universidad();
+
+        Alumno alumno1 = new Alumno("Adrian Arias", 17, Carreras.Ing_Electronica, "A123");
+        alumno1.AgregarCalificacion(50);
+        alumno1.AgregarCalificacion(0);
+        alumno1.AgregarCalificacion(25);
+        universidad.AgregarAlumno(alumno1);
+
+        Console.WriteLine("Alumnos:");
+        foreach (var alumno in universidad.ObtenerListaAlumnos())
+        {
+            string estado = alumno.EstadoAprobacion() == "aprobado" ? "Aprobado" : "Reprobado";
+            Console.WriteLine($"Nombre: {alumno.Nombre}, Edad: {alumno.Edad}, Carrera: {alumno.Carrera}, Promedio: {alumno.CalcularPromedio()}, Estado: {estado}, Carnet: {alumno.ObtenerNumeroCarnet()}");
+        }
+    }
+}
